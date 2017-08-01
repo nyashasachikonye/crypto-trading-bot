a5b451a7ae2c2e5320cd7fd7c514398c323052b4
Kuziwa Sachikonye <sach@sach.co.za>
2017-08-01 17:46:33 +0200

    writing to file called database.txt

---
+++ BTCCJoy.py
@@ -0,0 +1,4 @@
+print "Welcome to BTCC Joy!"
+x = input("BTC Price? ")
+y = float(960*(1+((x-37000)/37000)))
+print y
--- trading-bot.py
+++ trading-bot.py
@@ -1,7 +1,7 @@
 import time
 import sys, getopt
 import datetime
-import requests
+#import requests
 import urllib
 import urllib2
 import json
@@ -15,13 +15,19 @@ def main(argv):
 	data = json.loads(ret.read())
 
 	currentHoldings = ["BTC_AMP","BTC_BLK","BTC_BTC","BTC_BTS","BTC_DGB","BTC_ETC","BTC_ETH","BTC_GAME","BTC_LTC","BTC_NAUT","BTC_SYS","BTC_XRP"]
+	fh = open("database.txt", "a")
 	for item in data:
 		if item in currentHoldings:
 			percentChange = data[item]["percentChange"]
 			if (percentChange > 0):
-				print "currency_pair:",item
-				print "lastPrice:",data[item]["last"]
-				print "% change",data[item]["percentChange"],"\n"
+				fh.write("currency_pair: ")
+				fh.write(item)
+				fh.write("\n lastPrice: ")
+				fh.write(data[item]["last"])
+				fh.write("\n % change ")
+				fh.write(data[item]["percentChange"])
+				fh.write("\n")
+	fh.close
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
